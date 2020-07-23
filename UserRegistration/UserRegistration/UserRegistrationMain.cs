7037be5e101a1b5bec86003b3eecd660719ec402
mrunal93 <mrn26893@gmail.com>
2020-07-23 09:42:04 +0530

    [Mrunal]Add: welcome message and added the git ignore file to ignore some files

---
+++ UserRegistration/UserRegistration/UserRegistrationMain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    class UserRegistrationMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Simulation");
+        }
+    }
+}
